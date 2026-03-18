34cfb8157e0465a3f3373a209e1d8401341e943c
Will_Langley <langley1809@gmail.com>
2026-03-18 14:30:11 -0700

    No methods yet

---
+++ src/main/java/Book.java
@@ -0,0 +1,24 @@
+import java.time.LocalTime;
+
+/**
+ * This is the
+ * @author William Langley
+ */
+
+public class Book {
+    public static final int ISBN_ = 0;
+    public static final int TITLE_ = 1;
+    public static final int SUBJECT_ = 2;
+    public static final int PAGE_COUNT_ = 3;
+    public static final int AUTHOR_ = 4;
+    public static final int DUE_DATE_ = 5;
+
+    private String isbn;
+    private String title;
+    private String subject;
+    private int pageCount;
+    private String author;
+    private LocalTime dueDate;
+
+
+}
