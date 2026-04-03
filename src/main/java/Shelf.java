88557aba85a7516289f537a8ab825400bdc55223
Will_Langley <langley1809@gmail.com>
2026-04-03 11:50:52 -0700

    Shelf fully completed with all tests passing

--- src/main/java/Shelf.java
+++ src/main/java/Shelf.java
@@ -1,3 +1,4 @@
+import Utilities.Code;
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -5,12 +6,11 @@ public class Shelf {
     public int SHELF_NUMBER_;
     public int SUBJECT_;
 
-    private HashMap<Book, Integer> books;
+    private HashMap<Book, Integer> books = new HashMap<>();
     private int shelfNumber;
     private String subject;
 
     public Shelf() {
-
     }
 
     public Shelf(int shelfNumber, String subject) {
@@ -18,21 +18,55 @@ public class Shelf {
         this.subject = subject;
     }
 
+    public Code addBook(Book book) {
+        if (!subject.equals(book.getSubject())) {
+            return Code.SHELF_SUBJECT_MISMATCH_ERROR;
+        }else if (books.containsKey(book)) {
+            Integer temp = books.get(book);
+            temp++;
+            books.put(book, temp);
+            return Code.SUCCESS;
+        } else {
+            books.put(book, 1);
+            System.out.println(book + " added to shelf");
+            return Code.SUCCESS;
+        }
+    }
 
+    public int getBookCount(Book book) {
+        if (!books.containsKey(book))
+            return -1;
+        return books.get(book);
+    }
 
+    public Code removeBook(Book book) {
+        if (!books.containsKey(book)) {
+            System.out.println(book.getTitle() + " is not on shelf " + subject);
+            return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+        } else if (books.containsKey(book) && (books.get(book) == 0)) {
+            System.out.println("No copies of " + book.getTitle() + " remains on shelf " + subject);
+            return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+        } else {
+            Integer temp = books.get(book);
+            temp--;
+            books.put(book, temp);
+            System.out.println(book.getTitle() + " successfully removed from shelf " + subject);
+            return Code.SUCCESS;
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    public String listBooks() {
+        if (books == null || books.isEmpty())
+            return "0 books on shelf: " + shelfNumber + " : " + subject;
+        int totalBookCount = 0;
+        String bookInfo = "";
+        for(Book b: books.keySet()) {
+            int count = books.get(b);
+            totalBookCount += count;
+            bookInfo += "\n" + b.toString() + " " + count;
+        }
+        return totalBookCount + " books on shelf : " + shelfNumber+ " : " + subject + bookInfo;
+    }
 
     public int getSHELF_NUMBER_() {
         return SHELF_NUMBER_;
